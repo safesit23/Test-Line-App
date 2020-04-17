e8565199d651063ea314a5c4f68ac7407dedf9ec
Jatawat Xie <jatawatsafe@hotmail.com>
2020-04-18 01:16:06 +0700

    [UPDATE] add flex message function

--- src/components/testLiff.vue
+++ src/components/testLiff.vue
@@ -24,6 +24,10 @@
         <h6>Function ส่ง Sticker</h6>
         <b-button variant="success" @click="sendSticker()">SEND</b-button>
       </b-col>
+      <b-col sm="12">
+        <h6>Function ส่ง Flex Message</h6>
+        <b-button variant="success" @click="sendFlex()">SEND</b-button>
+      </b-col>
     </b-row>
   </b-container>
 </template>
@@ -96,6 +100,334 @@ export default {
             type: "sticker",
             packageId: "11537",
             stickerId: "52002762"
+          }
+        ])
+        .then(() => {
+          console.log("message sent");
+        })
+        .catch(err => {
+          console.log("error", err);
+        });
+    },
+    sendFlex() {
+      liff
+        .sendMessages([
+          {
+            type: "flex",
+            altText: "this is a flex message",
+            contents: {
+              type: "bubble",
+              size: "mega",
+              header: {
+                type: "box",
+                layout: "vertical",
+                contents: [
+                  {
+                    type: "box",
+                    layout: "vertical",
+                    contents: [
+                      {
+                        type: "text",
+                        text: "Stage",
+                        color: "#ffffff66",
+                        size: "sm"
+                      },
+                      {
+                        type: "text",
+                        text: "Main stage",
+                        color: "#ffffff",
+                        size: "xl",
+                        flex: 4,
+                        weight: "bold"
+                      }
+                    ]
+                  }
+                ],
+                paddingAll: "20px",
+                backgroundColor: "#4A67DC",
+                spacing: "md",
+                height: "100px",
+                paddingTop: "22px"
+              },
+              body: {
+                type: "box",
+                layout: "vertical",
+                contents: [
+                  {
+                    type: "text",
+                    text: "Total: 1.30 hour",
+                    color: "#b7b7b7",
+                    size: "xs"
+                  },
+                  {
+                    type: "box",
+                    layout: "horizontal",
+                    contents: [
+                      {
+                        type: "text",
+                        text: "19.00",
+                        size: "sm",
+                        gravity: "center"
+                      },
+                      {
+                        type: "box",
+                        layout: "vertical",
+                        contents: [
+                          {
+                            type: "filler"
+                          },
+                          {
+                            type: "box",
+                            layout: "vertical",
+                            contents: [
+                              {
+                                type: "filler"
+                              }
+                            ],
+                            cornerRadius: "30px",
+                            height: "12px",
+                            width: "12px",
+                            borderColor: "#EF454D",
+                            borderWidth: "2px"
+                          },
+                          {
+                            type: "filler"
+                          }
+                        ],
+                        flex: 0
+                      },
+                      {
+                        type: "text",
+                        text: "Session 1",
+                        gravity: "center",
+                        flex: 4,
+                        size: "sm"
+                      }
+                    ],
+                    spacing: "lg",
+                    cornerRadius: "30px",
+                    margin: "xl"
+                  },
+                  {
+                    type: "box",
+                    layout: "horizontal",
+                    contents: [
+                      {
+                        type: "box",
+                        layout: "baseline",
+                        contents: [
+                          {
+                            type: "filler"
+                          }
+                        ],
+                        flex: 1
+                      },
+                      {
+                        type: "box",
+                        layout: "vertical",
+                        contents: [
+                          {
+                            type: "box",
+                            layout: "horizontal",
+                            contents: [
+                              {
+                                type: "filler"
+                              },
+                              {
+                                type: "box",
+                                layout: "vertical",
+                                contents: [
+                                  {
+                                    type: "filler"
+                                  }
+                                ],
+                                width: "2px",
+                                backgroundColor: "#B7B7B7"
+                              },
+                              {
+                                type: "filler"
+                              }
+                            ],
+                            flex: 1
+                          }
+                        ],
+                        width: "12px"
+                      },
+                      {
+                        type: "text",
+                        text: "30 Mins",
+                        gravity: "center",
+                        flex: 4,
+                        size: "xs",
+                        color: "#8c8c8c"
+                      }
+                    ],
+                    spacing: "lg",
+                    height: "64px"
+                  },
+                  {
+                    type: "box",
+                    layout: "horizontal",
+                    contents: [
+                      {
+                        type: "box",
+                        layout: "horizontal",
+                        contents: [
+                          {
+                            type: "text",
+                            text: "20:00",
+                            gravity: "center",
+                            size: "sm"
+                          }
+                        ],
+                        flex: 1
+                      },
+                      {
+                        type: "box",
+                        layout: "vertical",
+                        contents: [
+                          {
+                            type: "filler"
+                          },
+                          {
+                            type: "box",
+                            layout: "vertical",
+                            contents: [
+                              {
+                                type: "filler"
+                              }
+                            ],
+                            cornerRadius: "30px",
+                            width: "12px",
+                            height: "12px",
+                            borderWidth: "2px",
+                            borderColor: "#6486E3"
+                          },
+                          {
+                            type: "filler"
+                          }
+                        ],
+                        flex: 0
+                      },
+                      {
+                        type: "text",
+                        text: "Session 2",
+                        gravity: "center",
+                        flex: 4,
+                        size: "sm"
+                      }
+                    ],
+                    spacing: "lg",
+                    cornerRadius: "30px"
+                  },
+                  {
+                    type: "box",
+                    layout: "horizontal",
+                    contents: [
+                      {
+                        type: "box",
+                        layout: "baseline",
+                        contents: [
+                          {
+                            type: "filler"
+                          }
+                        ],
+                        flex: 1
+                      },
+                      {
+                        type: "box",
+                        layout: "vertical",
+                        contents: [
+                          {
+                            type: "box",
+                            layout: "horizontal",
+                            contents: [
+                              {
+                                type: "filler"
+                              },
+                              {
+                                type: "box",
+                                layout: "vertical",
+                                contents: [
+                                  {
+                                    type: "filler"
+                                  }
+                                ],
+                                width: "2px",
+                                backgroundColor: "#6486E3"
+                              },
+                              {
+                                type: "filler"
+                              }
+                            ],
+                            flex: 1
+                          }
+                        ],
+                        width: "12px"
+                      },
+                      {
+                        type: "text",
+                        text: "30 Mins",
+                        gravity: "center",
+                        flex: 4,
+                        size: "xs",
+                        color: "#8c8c8c"
+                      }
+                    ],
+                    spacing: "lg",
+                    height: "64px"
+                  },
+                  {
+                    type: "box",
+                    layout: "horizontal",
+                    contents: [
+                      {
+                        type: "text",
+                        text: "20:30",
+                        gravity: "center",
+                        size: "sm"
+                      },
+                      {
+                        type: "box",
+                        layout: "vertical",
+                        contents: [
+                          {
+                            type: "filler"
+                          },
+                          {
+                            type: "box",
+                            layout: "vertical",
+                            contents: [
+                              {
+                                type: "filler"
+                              }
+                            ],
+                            cornerRadius: "30px",
+                            width: "12px",
+                            height: "12px",
+                            borderColor: "#6486E3",
+                            borderWidth: "2px"
+                          },
+                          {
+                            type: "filler"
+                          }
+                        ],
+                        flex: 0
+                      },
+                      {
+                        type: "text",
+                        text: "Session 3",
+                        gravity: "center",
+                        flex: 4,
+                        size: "sm"
+                      }
+                    ],
+                    spacing: "lg",
+                    cornerRadius: "30px"
+                  }
+                ]
+              }
+            }
           }
         ])
         .then(() => {
